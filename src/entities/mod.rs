c61f15f62e2d50fce1cb19028a402743a7f82813
Erik Steinbach <73831083+eekkaiia@users.noreply.github.com>
2023-02-24 09:45:25 -0500

    Delete src directory

--- src/entities/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod theseus;
-pub mod paraphernalia;
-
-pub use self::theseus::*;
-pub use self::paraphernalia::*;
--- src/systems/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod stage;
-pub mod ecs;
-pub mod info;
-pub mod amaze;
-
-pub use self::stage::*;
-pub use self::ecs::*;
-pub use self::info::*;
-pub use self::amaze::*;
